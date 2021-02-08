9dc03be783b518d004fedc6601f400d785eb6073
RizTech <RizTech@DESKTOP-TO5NDE3>
2021-02-08 18:38:55 +0500

    Add libman to use bclient side library

--- RizTech/RizTech/Controllers/HomeController.cs
+++ RizTech/RizTech/Controllers/HomeController.cs
@@ -10,9 +10,16 @@ namespace RizTech.Controllers
     {
         public ViewResult Index()
         {
+            //var obj = new { Id = 1, Name = "Rizwan" };
+            //return View("AboutUs", obj);
+            // return View("../../TempView/RizwanTemp");
             return View();
         }
         public ViewResult AboutUs()
+        {
+            return View();
+        }
+        public ViewResult ContactUs()
         {
             return View();
         }
